cc48a42c9786c1602b8ce8b0b74330dd231b3918
Volkan <Home@Volkan.local>
2017-09-03 23:02:03 -0700

    full test passed

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,17 @@
-var ForagerBee = function() {
-};
 
+var ForagerBee = function() {
+
+  Bee.call(this);
+
+  this.age = 10;
+  this.canFly = true;
+  this.job = 'find pollen';
+  this.treasureChest = [];
+  };
+
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+};
